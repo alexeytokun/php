39565106e07ae284de63498288077282bdac5482
alexeytokun <alexeytokun@gmail.com>
2018-02-19 16:57:31 +0300

    task 1_1

---
+++ 1_1.php
@@ -0,0 +1,14 @@
+<?php
+    function sumDigits ($n) {
+      $sum = 0;
+      while($n >= 1) {
+        if(($n % 10) < 5) {
+          $sum += $n % 10;
+        }
+          $n /= 10;
+      }
+       return $sum;
+    }
+
+  echo sumDigits(12345);
+?>
